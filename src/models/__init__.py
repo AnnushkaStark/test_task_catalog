29f6b2e04efb753cf7478c62a72b1d8651d52e92
root <psk221219@gmail.com>
2025-04-10 20:55:04 +0300

    include models

---
+++ src/models/__init__.py
@@ -0,0 +1,15 @@
+from .color import Color
+from .height import Height
+from .m2m import ProductColor, ProductHeight, ProductMemorySize
+from .memory_size import MemorySize
+from .product import Product
+
+__all__ = [
+    "Color",
+    "Height",
+    "ProductColor",
+    "ProductHeight",
+    "ProductMemorySize",
+    "MemorySize",
+    "Product",
+]
