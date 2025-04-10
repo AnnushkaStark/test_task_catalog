249af0f338cd5346190bf20fcb2ea7e3e2d50bab
root <psk221219@gmail.com>
2025-04-10 21:05:31 +0300

    added height model

--- src/models/height.py
+++ src/models/height.py
@@ -6,29 +6,29 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from databases.database import Base
 
-from .m2m import ProductColor
+from .m2m import ProductHeight
 
 if TYPE_CHECKING:
     from .product import Product
 
 
-class Color(Base):
+class Height(Base):
     """
-    Модель цвета
+    Модель высоты
 
     ## Attrs:
-      - id: int - идентификатор
-      - uid: UUID - идентификатор
-      - value: str - обозначение цвета в RGB
-      - products: List[Product] - связь с продуктами
+        - id: int - идентифкатор
+        - uid: UUID - идентификатор
+        - value: int - высота
+        - products: List[Product] - связь с продуктами
     """
 
-    __tablename__ = "color"
+    __tablename__ = "height"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     uid: Mapped[uuid.UUID] = mapped_column(
         UUID, unique=True, index=True, default=uuid.uuid4
     )
-    value: Mapped[str]
+    value: Mapped[int]
     products: Mapped[List["Product"]] = relationship(
-        "Product", back_populates="colors", secondary=ProductColor.__table__
+        "Product", back_populates="heights", secondary=ProductHeight.__table__
     )
