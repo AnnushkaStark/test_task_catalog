8dbc1fbc599dbd3ff8da1ca6df07127bb675c81d
root <psk221219@gmail.com>
2025-04-11 09:03:05 +0300

    update read async

--- src/crud/crud_mixins/read.py
+++ src/crud/crud_mixins/read.py
@@ -1,8 +1,8 @@
-from typing import Generic, List, Optional
+from typing import Any, Generic, List, Optional, Sequence
 from uuid import UUID
 
+from sqlalchemy import func, select
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.future import select
 
 from constants.crud_types import ModelType
 
@@ -23,3 +23,28 @@ class ReadAsync(Generic[ModelType]):
         statement = select(self.model).where(self.model.id.in_(ids))
         result = await db.execute(statement)
         return result.scalars().all()
+
+    async def get_by_value(
+        self, db: AsyncSession, value: Any
+    ) -> Optional[ModelType]:
+        statement = select(self.model).where(self.model.value == value)
+        result = await db.execute(statement)
+        return result.scalars().first()
+
+    async def get_multi(
+        self, db: AsyncSession, skip: int = 0, limit: int = 10
+    ) -> Sequence[ModelType]:
+        statement = (
+            select(self.model, func.count().over().label("total"))
+            .offset(skip)
+            .limit(limit)
+        )
+        result = await db.execute(statement)
+        result = await db.execute(statement)
+        rows = result.mappings().unique().all()
+        return {
+            "limit": limit,
+            "offset": skip,
+            "total": rows[0]["total"] if rows else 0,
+            "objects": [r[f"{self.model}"] for r in rows],
+        }
