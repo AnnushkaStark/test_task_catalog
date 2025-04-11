7ebf2662847159f8945c1541999be13e5cc07bf7
root <psk221219@gmail.com>
2025-04-11 09:25:53 +0300

    update peoduct crud

--- src/crud/product.py
+++ src/crud/product.py
@@ -1,9 +1,9 @@
 from typing import Optional
 from uuid import UUID
 
+from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.future import select
-from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import joinedload, selectinload
 
 from crud.async_crud import BaseAsyncCRUD
 from models import Product
@@ -14,13 +14,39 @@ class ProductCRUD(BaseAsyncCRUD[Product, ProductBase, ProductCreate]):
     async def get_by_uid(
         self, db: AsyncSession, uid: UUID
     ) -> Optional[Product]:
-        statement = select(self.model).options(
-            joinedload(self.model.heights),
-            joinedload(self.model.colors),
-            joinedload(self.model.memory_sizes),
+        statement = (
+            select(self.model)
+            .options(
+                joinedload(self.model.heights),
+                joinedload(self.model.colors),
+                joinedload(self.model.memory_sizes),
+            )
+            .where(self.model.uid == uid)
         )
         result = await db.execute(statement)
         return result.scalars().unique().first()
+
+    async def create(
+        self,
+        db: AsyncSession,
+        create_schema: ProductBase,
+        commit: bool = True,
+    ) -> Product:
+        data = create_schema.model_dump(exclude_unset=True)
+        stmt = (
+            insert(self.model)
+            .values(**data)
+            .returning(self.model)
+            .options(
+                selectinload(self.model.heights),
+                selectinload(self.model.colors),
+                selectinload(self.model.memory_sizes),
+            )
+        )
+        res = await db.execute(stmt)
+        if commit:
+            await db.commit()
+        return res.scalars().first()
 
 
 product_crud = ProductCRUD(Product)
